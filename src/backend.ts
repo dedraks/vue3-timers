3fba5f6500fe3d45a6d82b711ec868a0bb80bb27
Carlos Alberto Borges Garcia Junior <dedraks@gmail.com>
2024-09-26 13:37:18 -0300

    ♻️ Format with prettier

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,4 @@
-var intervalIds: {[key: number]: any} = {}
+var intervalIds: { [key: number]: any } = {};
 
 self.onmessage = function (e) {
   switch (e.data.command) {
@@ -6,27 +6,27 @@ self.onmessage = function (e) {
       var intvalId = setInterval(function () {
         postMessage({
           message: 'interval:tick',
-          id: e.data.id
-        })
-      }, e.data.interval)
+          id: e.data.id,
+        });
+      }, e.data.interval);
 
       postMessage({
         message: 'interval:started',
-        id: e.data.id
-      })
+        id: e.data.id,
+      });
 
-      intervalIds[e.data.id] = intvalId
-      break
+      intervalIds[e.data.id] = intvalId;
+      break;
 
     case 'interval:clear':
-      clearInterval(intervalIds[e.data.id])
+      clearInterval(intervalIds[e.data.id]);
 
       postMessage({
         message: 'interval:cleared',
-        id: e.data.id
-      })
+        id: e.data.id,
+      });
 
-      delete intervalIds[e.data.id]
-      break
+      delete intervalIds[e.data.id];
+      break;
   }
-}
+};
--- src/timer.ts
+++ src/timer.ts
@@ -1,47 +1,51 @@
 //var worker = new Worker('./timer-worker.js')
 
-
-
-var worker = new Worker(new URL('backend.ts', import.meta.url))
+var worker = new Worker(new URL('backend.ts', import.meta.url));
 var vue3Timer = {
   id: 0,
-  callbacks: [{fn: Function, context: undefined}],
+  callbacks: [{ fn: Function, context: undefined }],
 
-  setInterval: function (cb: any, interval: number, context= undefined, oneTime=false) {
-    this.id++
-    var id = this.id
-    this.callbacks[id] = { fn: cb, context: context }
+  setInterval: function (
+    cb: any,
+    interval: number,
+    context = undefined,
+    oneTime = false,
+  ) {
+    this.id++;
+    var id = this.id;
+    this.callbacks[id] = { fn: cb, context: context };
     worker.postMessage({
       command: 'interval:start',
       interval: interval,
       id: id,
-      oneTime: oneTime
-    })
-    return id
+      oneTime: oneTime,
+    });
+    return id;
   },
 
-  setTimeout: function (cb: any, interval: number, context=undefined) {
+  setTimeout: function (cb: any, interval: number, context = undefined) {
     this.setInterval(cb, interval, context, true);
   },
 
   onMessage: function (e: any) {
     switch (e.data.message) {
       case 'interval:tick':
-        var callback = this.callbacks[e.data.id]
-        if (callback && callback.fn) callback.fn.apply(callback.context)
-        if (e.data.message.oneTime) worker.postMessage({ command: 'interval:clear', id: e.data.id })
-        break
+        var callback = this.callbacks[e.data.id];
+        if (callback && callback.fn) callback.fn.apply(callback.context);
+        if (e.data.message.oneTime)
+          worker.postMessage({ command: 'interval:clear', id: e.data.id });
+        break;
       case 'interval:cleared':
-        delete this.callbacks[e.data.id]
-        break
+        delete this.callbacks[e.data.id];
+        break;
     }
   },
 
   clearInterval: function (id: number) {
-    worker.postMessage({ command: 'interval:clear', id: id })
-  }
-}
+    worker.postMessage({ command: 'interval:clear', id: id });
+  },
+};
 
-worker.onmessage = vue3Timer.onMessage.bind(vue3Timer)
+worker.onmessage = vue3Timer.onMessage.bind(vue3Timer);
 
-export default vue3Timer
+export default vue3Timer;
